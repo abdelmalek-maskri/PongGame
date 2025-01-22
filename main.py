db420a0d79d9ab913e5d49dfea75c419f4cde664
Abdelmalek Maskri <abdelmalekmaskri18@gmail.com>
2025-01-22 18:49:02 +0000

    added the ball to the game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
-
+from ball import Ball
+import time 
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -11,6 +12,8 @@ screen.tracer(0)
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 
+ball = Ball()
+
 screen.listen()
 screen.onkey(r_paddle.goUp, "Up")
 screen.onkey(r_paddle.goDown, "Down")
@@ -20,7 +23,9 @@ screen.onkey(l_paddle.goDown, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 
